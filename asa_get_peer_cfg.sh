52ffde0c0b11c39bd3cfc63b1ecc571ba6c86640
Eugene M <ujin@i.ua>
2018-09-11 12:18:22 +0300

    second commit

---
+++ asa_get_peer_cfg.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+
+CONFIG=$1
+PEER=$2
+
+if [ -n "${PEER}" ] && [ -n "${CONFIG}" ]
+then
+
+	CRYPTOMAP=`pcregrep --only-matching=1 "^crypto\s+map\s+(\S+\s+\S+).+\s+${PEER}" ${CONFIG}`
+	ACL=`pcregrep --only-matching=1 "^crypto\s+map\s+${CRYPTOMAP}\s+match\s+address\s+(\S+)" ${CONFIG}`
+
+	if [ -n "${CRYPTOMAP}" ] && [ -n "${ACL}" ] 
+		then
+			echo '###########################################'
+			pcregrep "^access-list\s+$ACL\s.+" ${CONFIG}
+			pcregrep -M "^tunnel-group\s${PEER}(.|\n\s)+" ${CONFIG}
+			pcregrep "^crypto\s+map\s+${CRYPTOMAP}.+" ${CONFIG}
+			echo '###########################################'
+		else
+			echo "${PEER} - not found"
+		fi
+else
+	echo "Get ipcec config part from file"
+	echo "getpeer.sh [file] [peer]"
+fi
